bd21200a76544c8246012fb5f75c7c4fbf71c438
asylum29 <ksenon3@mail.ru>
2021-11-12 14:53:32 +0700

    fix

--- grpc-client.go
+++ grpc-client.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"context"
-	"google.golang.org/grpc"
-	"log"
-	"os"
-	"workplace/config"
-	gen "workplace/proto"
+    "context"
+    "google.golang.org/grpc"
+    "log"
+    "os"
+    "workplace/config"
+    gen "workplace/proto"
 )
 
 func main() {
-	configuration := config.GetConfig()
-	address := configuration.GrpcHost + ":" + configuration.GrpcPort
-	conn, _ := grpc.Dial(address, grpc.WithInsecure())
-	client := gen.NewTestServiceClient(conn)
-
-	name := os.Args[1]
-	resp, err := client.Do(context.Background(), &gen.Request{Name: name})
-	if err != nil {
-		log.Fatalf("could not get answer: %v", err)
-	}
-
-	log.Println("Response:", resp.Message)
+    configuration := config.GetConfig()
+    address := configuration.GrpcHost + ":" + configuration.GrpcPort
+    conn, _ := grpc.Dial(address, grpc.WithInsecure())
+    client := gen.NewTestServiceClient(conn)
+    
+    name := os.Args[1]
+    resp, err := client.Do(context.Background(), &gen.Request{Name: name})
+    if err != nil {
+        log.Fatalf("could not get answer: %v", err)
+    }
+    
+    log.Println("Response:", resp.Message)
 }
--- grpc-server.go
+++ grpc-server.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"google.golang.org/grpc"
-	"log"
-	"net"
-	"workplace/config"
-	gen "workplace/proto"
+    "google.golang.org/grpc"
+    "log"
+    "net"
+    "workplace/config"
+    gen "workplace/proto"
 )
 
 func main() {
-	server := grpc.NewServer()
-
-	instance := new(gen.TestService)
-
-	gen.RegisterTestServiceServer(server, instance)
-
-	configuration := config.GetConfig()
-	address := configuration.GrpcHost + ":" + configuration.GrpcPort
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatal("Unable to create grpc listener:", err)
-	}
-
-	if err = server.Serve(listener); err != nil {
-		log.Fatal("Unable to start server:", err)
-	}
+    server := grpc.NewServer()
+    
+    instance := new(gen.TestService)
+    
+    gen.RegisterTestServiceServer(server, instance)
+    
+    configuration := config.GetConfig()
+    address := configuration.GrpcHost + ":" + configuration.GrpcPort
+    listener, err := net.Listen("tcp", address)
+    if err != nil {
+        log.Fatal("Unable to create grpc listener:", err)
+    }
+    
+    if err = server.Serve(listener); err != nil {
+        log.Fatal("Unable to start server:", err)
+    }
 }
