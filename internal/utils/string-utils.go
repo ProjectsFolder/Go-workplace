f60a9089bab79a7e1227b362f69a5205ba023be6
asylum29 <ksenon3@mail.ru>
2021-12-16 17:08:45 +0700

    fix ChunkSplit function

--- internal/utils/string-utils.go
+++ internal/utils/string-utils.go
@@ -1,7 +1,10 @@
 package stringUtils
 
+import "math"
+
 func ChunkSplit(s string, l int) []string {
-    var result []string
+    i := 0
+    result := make([]string, int(math.Ceil(float64(len(s)) / float64(l))))
     for len(s) > 0 {
         var length int
         if len(s) > l {
@@ -10,7 +13,8 @@ func ChunkSplit(s string, l int) []string {
             length = len(s)
         }
         temp := s[:length]
-        result = append(result, temp)
+        result[i] = temp
+        i += 1
         s = s[length:]
     }
 
